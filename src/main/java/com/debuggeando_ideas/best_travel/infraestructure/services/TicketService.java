590dc9eefa1b5c85ed9e2c1dc6a0ad74f21055c8
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-13 11:01:35 -0400

    creando DTOs de request y response para fly y ticket

---
+++ src/main/java/com/debuggeando_ideas/best_travel/infraestructure/services/TicketService.java
@@ -0,0 +1,76 @@
+package com.debuggeando_ideas.best_travel.infraestructure.services;
+
+import com.debuggeando_ideas.best_travel.api.models.request.TicketRequest;
+import com.debuggeando_ideas.best_travel.api.models.response.FlyResponse;
+import com.debuggeando_ideas.best_travel.api.models.response.TicketResponse;
+import com.debuggeando_ideas.best_travel.domain.entities.jpa.TicketEntity;
+import com.debuggeando_ideas.best_travel.domain.repositories.jpa.CustomerRepository;
+import com.debuggeando_ideas.best_travel.domain.repositories.jpa.FlyRepository;
+import com.debuggeando_ideas.best_travel.domain.repositories.jpa.TicketRepository;
+import com.debuggeando_ideas.best_travel.infraestructure.abstract_services.ITicketService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Transactional
+@Service
+@Slf4j
+@AllArgsConstructor
+public class TicketService implements ITicketService {
+
+    private final FlyRepository flyRepository;
+    private final TicketRepository ticketRepository;
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public TicketResponse created(TicketRequest request) {
+        var fly = flyRepository.findById(request.getIdFly()).orElseThrow();
+        var customer = customerRepository.findById(request.getIdClient()).orElseThrow();
+
+        var ticketToPersist = TicketEntity.builder()
+                .id(UUID.randomUUID())
+                .fly(fly)
+                .customer(customer)
+                .price(fly.getPrice())
+                .purchaseDate(LocalDate.now())
+                .arrivalDate(LocalDateTime.now())
+                .departureDate(LocalDateTime.now())
+                .build();
+
+        var ticketPersisted = ticketRepository.save(ticketToPersist);
+
+        return entityToResponse(ticketPersisted);
+    }
+
+    @Override
+    public TicketResponse read(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public TicketResponse update(TicketRequest request, UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public void delete(UUID uuid) {
+
+    }
+
+    private TicketResponse entityToResponse(TicketEntity entity) {
+        var response = new TicketResponse();
+        BeanUtils.copyProperties(entity,response);
+
+        var flyResponse = new FlyResponse();
+        BeanUtils.copyProperties(entity.getFly(),flyResponse);
+        response.setFly(flyResponse);
+
+        return response;
+    }
+}
