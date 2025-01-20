1a3a25df5b67bdeacba61676e5e6327f4735e261
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-19 23:09:58 -0400

    agregando methodArgumentNotValidException al restControllerAdvice

--- src/main/java/com/debuggeando_ideas/best_travel/api/error_handler/BadRequestController.java
+++ src/main/java/com/debuggeando_ideas/best_travel/api/error_handler/BadRequestController.java
@@ -2,11 +2,15 @@ package com.debuggeando_ideas.best_travel.api.error_handler;
 
 import com.debuggeando_ideas.best_travel.api.models.response.BaseErrorResponse;
 import com.debuggeando_ideas.best_travel.api.models.response.ErrorResponse;
+import com.debuggeando_ideas.best_travel.api.models.response.ErrorsResponse;
 import com.debuggeando_ideas.best_travel.util.exceptions.IdNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
 
 @RestControllerAdvice
 @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -21,4 +25,16 @@ public class BadRequestController {
                 .build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseErrorResponse handleIdNotFound(MethodArgumentNotValidException exception) {
+        var errors = new ArrayList<String>();
+        exception.getAllErrors()
+                .forEach(error -> errors.add(error.getDefaultMessage()));
+
+        return ErrorsResponse.builder()
+                .errors(errors)
+                .status(HttpStatus.BAD_REQUEST.name())
+                .code(HttpStatus.BAD_REQUEST.value())
+                .build();
+    }
 }
