31bf05bda6f363cd5b194eff1ea4e228d6789e5f
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-24 20:38:18 -0400

    Creado repository e interface para users

--- src/main/java/com/debuggeando_ideas/best_travel/domain/repositories/mongo/AppUserRepository.java
+++ src/main/java/com/debuggeando_ideas/best_travel/domain/repositories/mongo/AppUserRepository.java
@@ -2,5 +2,9 @@ package com.debuggeando_ideas.best_travel.domain.repositories.mongo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface AppUserRepository extends MongoRepository<AppUserRepository, String> {
+
+    Optional<AppUserRepository> findByUsername(String username);
 }
