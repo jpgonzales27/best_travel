badb55d09cc6fc68ad6593ae63592cea7131f54f
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-24 21:53:39 -0400

    Fixeando AppUserRepository

--- src/main/java/com/debuggeando_ideas/best_travel/domain/repositories/mongo/AppUserRepository.java
+++ src/main/java/com/debuggeando_ideas/best_travel/domain/repositories/mongo/AppUserRepository.java
@@ -1,10 +1,12 @@
 package com.debuggeando_ideas.best_travel.domain.repositories.mongo;
 
+import com.debuggeando_ideas.best_travel.domain.entities.documents.AppUserDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface AppUserRepository extends MongoRepository<AppUserRepository, String> {
+public interface AppUserRepository extends MongoRepository<AppUserDocument, String> {
 
-    Optional<AppUserRepository> findByUsername(String username);
+    Optional<AppUserDocument> findByUsername(String username);
 }
+
